5bc5493fca516af5287b5ac4fa75cf1e43467dd6
Betsy <betrakiss@hotmail.com>
2014-10-26 18:25:26 +0200

    Adding Operator class.

---
+++ HomeWork 1/HomeWork 1/Operator.cpp
@@ -0,0 +1,4 @@
+#include "Operator.h"
+
+Operator::Operator(char alpha, char sgn, int prior, int assoc) : letter(alpha), sign(sgn), priority(prior), associativity(assoc)
+{ }
--- HomeWork 1/HomeWork 1/Operator.h
+++ HomeWork 1/HomeWork 1/Operator.h
@@ -1,7 +1,23 @@
 #ifndef OPERATOR_H
 #define OPERATOR_H
 
+class Operator
+{
+public:
+	Operator(char, char, int, int);
 
+public:
+	char getLetter() const { return letter; }
+	char getSign() const { return sign; }
+	int getPriority() const { return priority; }
+	int getAssociativity() const { return associativity; }
+
+private:
+	char letter;
+	char sign;
+	int priority;
+	int associativity;
+};
 
 
 #endif // OPERATOR_H
